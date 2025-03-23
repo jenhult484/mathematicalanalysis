f671441a0f98a5f0a2c73e63e85b2e0fb59a2e32
jenhult484 <jenhult484@gmail.com>
2025-03-23 16:24:13 +0000

    Add 18.rs

---
+++ src/18.rs
@@ -0,0 +1,7 @@
+// This is an example of Rust programming.
+// In this example, we're exploring the concept of functions and their usage in Rust.
+
+fn main() {
+    // Example function that prints "Hello, world!" to the console
+    println!("Hello, world!");
+}
